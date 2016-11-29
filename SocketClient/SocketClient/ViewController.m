cd1056495d0615f5639a214748cfa5259ce21c8a
YWSawyer <yuan_wei_1@163.com>
2016-11-29 18:01:09 +0800

    finish socket connect

--- SocketClient/SocketClient/ViewController.m
+++ SocketClient/SocketClient/ViewController.m
@@ -15,6 +15,8 @@
 @property (weak, nonatomic) IBOutlet UITextField *port;
 @property (weak, nonatomic) IBOutlet UITextView *errorText;
 
+@property (weak, nonatomic) IBOutlet UIButton *sendBtn;
+@property (weak, nonatomic) IBOutlet UITextField *sendText;
 @property (nonatomic) GCDAsyncSocket *socket;
 
 @end
@@ -26,8 +28,9 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     _socket = [[GCDAsyncSocket alloc]initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
-    _host.text = @"23.33.22";
-    _port.text = @"8080";
+    _host.text = @"192.168.40.142";
+    _port.text = @"8808";
+    _sendText.text = @"hello world!";
 }
 
 
@@ -40,25 +43,58 @@
     NSError *error = nil;
     BOOL result;
     
-    result = [self.socket connectToHost:self.host.text onPort:[self.port.text integerValue] error:&error];
+    result = [self.socket connectToHost:self.host.text onPort:[self.port.text integerValue] withTimeout:-1 error:&error];
     self.errorText.text = [NSString stringWithFormat:@"result:%d, error:%@",result,[error localizedDescription]];
 }
 - (IBAction)disconect:(id)sender {
     
     [self.socket disconnect];
+    //断开连接仅仅是断开连接了，但socket的引用还是存在的，如果直接发送数据还是可以发送出去的，需要直接释放引用。
+    self.socket = nil;
+}
+- (IBAction)sendData:(UIButton *)sender {
+    
+    NSData *data = [self.sendText.text dataUsingEncoding:NSUTF8StringEncoding];
+    [self.socket writeData:data withTimeout:-1 tag:999];
+    
 }
 
 - (void)socket:(GCDAsyncSocket *)sock didConnectToHost:(NSString *)host port:(uint16_t)port {
     
-     self.errorText.text = [NSString stringWithFormat:@"new socket:%@",host];
+    self.errorText.text = [NSString stringWithFormat:@"didConnectToHost:%@",host];
+    //建立连接后，开始无超时的读取新Socket发送的数据，如果没有数据一直等待，如果有数据didReadData回调被吊起。（不主动读取socket数据是读不到任何数据的）
+    [self.socket readDataWithTimeout:-1 tag:0];
      NSLog(@"%@",self.errorText.text);
 }
 
 - (void)socketDidDisconnect:(GCDAsyncSocket *)sock withError:(nullable NSError *)err{
     
-    self.errorText.text = [NSString stringWithFormat:@"disconnect error:%@",[err localizedDescription]];
+    self.errorText.text = [NSString stringWithFormat:@"DidDisconnect error:%@",[err localizedDescription]];
     NSLog(@"%@",self.errorText.text);
 }
+
+- (void)socket:(GCDAsyncSocket *)sock didWriteDataWithTag:(long)tag {
+    NSDateFormatter *format = [NSDateFormatter new];
+    format.dateFormat = @"HH:mm:ss";
+    NSString *time = [format stringFromDate:[NSDate date]];
+    self.errorText.text = [NSString stringWithFormat:@"[%@]send data successfully",time];
+    NSLog(@"%@",self.errorText.text);
+
+}
+
+- (void)socket:(GCDAsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag {
+    
+    NSDateFormatter *format = [NSDateFormatter new];
+    format.dateFormat = @"HH:mm:ss";
+    NSString *time = [format stringFromDate:[NSDate date]];
+    NSString *receiveText = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
+    self.errorText.text = [NSString stringWithFormat:@"[%@]%@: %@",time,sock.connectedHost,receiveText];
+    //读取完newSocket发送的数据后，需要再次主动读取数据，否则didReadData不会再次调起。
+    [self.socket readDataWithTimeout:-1 tag:0];
+    NSLog(@"%@",self.errorText.text);
+}
+
+
 
 
 
--- SocketMacServer/SocketServer/ViewController.m
+++ SocketMacServer/SocketServer/ViewController.m
@@ -13,8 +13,11 @@
 @property (weak) IBOutlet NSTextField *host;
 @property (weak) IBOutlet NSTextField *port;
 @property (weak) IBOutlet NSTextView *statusTips;
+@property (weak) IBOutlet NSButton *lisBtn;
+@property (weak) IBOutlet NSTextField *sendText;
 
 @property (nonatomic) GCDAsyncSocket *socket;
+@property (nonatomic) GCDAsyncSocket *serverSocket;
 
 @end
 
@@ -26,8 +29,10 @@
     // Do any additional setup after loading the view.
     
     _socket = [[GCDAsyncSocket alloc]initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
-    _host.stringValue = @"localhost";
-    _port.stringValue = @"8080";
+    _host.stringValue = @"nil";
+    _port.stringValue = @"8808";
+    _statusTips.string = @"socket is ready";
+    _sendText.stringValue = @"how are you ?";
 }
 
 
@@ -41,10 +46,73 @@
     NSError *error = nil;
     BOOL result;
     
-    result = [self.socket acceptOnInterface:self.host.stringValue port:[self.port.stringValue integerValue] error:&error];
-    self.statusTips.contentView. = [NSString stringWithFormat:@"result:%d, error:%@",result,[error localizedDescription]];
+    result = [self.socket acceptOnInterface:nil port:[self.port.stringValue integerValue] error:&error];
+    
+    self.statusTips.string = [NSString stringWithFormat:@"result:%d, error:%@",result,[error localizedDescription]];
+    if (result) {
+        self.statusTips.string = @"start listenning...";
+    }
+}
+- (IBAction)disconnect:(NSButton *)sender {
+    
+    //断开连接需要使用newSocket断开，
+    [self.serverSocket disconnect];
+}
+- (IBAction)sendData:(NSButton *)sender {
+    
+    NSData *data = [self.sendText.stringValue dataUsingEncoding:NSUTF8StringEncoding];
+    if (self.serverSocket) {
+        //发送数据使用新连接的serverSocket
+        [self.serverSocket writeData:data withTimeout:-1 tag:0];
+    }else{
+        self.statusTips.string = @"no retain client socket!";
+    }
+    
+}
+//服务端开始在特定端口侦听后还没有真正的socket，直到和客户端建立连接后才会创建真正的newSocket即服务端socket。
+- (void)socket:(GCDAsyncSocket *)sock didAcceptNewSocket:(GCDAsyncSocket *)newSocket {
+    
+    self.statusTips.string = [NSString stringWithFormat:@"new socket:%@",newSocket.connectedHost];
+    //持有新创建的server端newSocket，不持有新连接的Socket，新的socket会自动被系统释放。详细说明看didAcceptNewSocket这个回调的说明。
+    self.serverSocket = newSocket;
+    //给新连接的Socket设置代理，共用代理。
+    self.serverSocket.delegate = self;
+    //建立连接后，开始无超时的读取新Socket发送的数据，如果没有数据一直等待，如果有数据didReadData回调被吊起。（不主动读取socket数据是读不到任何数据的）
+    [self.serverSocket readDataWithTimeout:-1 tag:0];
+    NSLog(@"%@",self.statusTips.string);
 
 }
+
+- (void)socketDidDisconnect:(GCDAsyncSocket *)sock withError:(nullable NSError *)err{
+    
+    self.statusTips.string = [NSString stringWithFormat:@"DidDisconnect error:%@",[err localizedDescription]];
+    NSLog(@"%@",self.statusTips.string);
+}
+
+
+- (void)socket:(GCDAsyncSocket *)sock didWriteDataWithTag:(long)tag {
+    
+    NSDateFormatter *format = [NSDateFormatter new];
+    format.dateFormat = @"HH:mm:ss";
+    NSString *time = [format stringFromDate:[NSDate date]];
+    self.statusTips.string = [NSString stringWithFormat:@"[%@]:send data successfully",time];
+    NSLog(@"%@",self.statusTips.string);
+    
+}
+
+- (void)socket:(GCDAsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag {
+    
+    NSDateFormatter *format = [NSDateFormatter new];
+    format.dateFormat = @"HH:mm:ss";
+    NSString *time = [format stringFromDate:[NSDate date]];
+    NSString *receiveText = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
+    self.statusTips.string = [NSString stringWithFormat:@"[%@]%@: %@",time,self.serverSocket.connectedHost,receiveText];
+    //读取完newSocket发送的数据后，需要再次主动读取数据，否则didReadData不会再次调起。
+    [self.serverSocket readDataWithTimeout:-1 tag:0];
+    NSLog(@"%@",self.statusTips.string);
+}
+
+
 
 
 @end
